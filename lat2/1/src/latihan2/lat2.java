4c9287b104577ee18d70c14e10402beae8325a2c
jimmyilyas30 <114792826+jimmyilyas30@users.noreply.github.com>
2023-10-08 15:50:00 +0700

    Add files via upload

---
+++ lat2/1/src/latihan2/lat2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int Number = 10;
+    char letter = 'a';
+    boolean result = true;
+String str = "hello";
+System.out.println("Number = " + Number);
+System.out.println("letter = " + letter);
+System.out.println("result = " + result);
+System.out.println("str = " + str);
+    }
+    
+}
